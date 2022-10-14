c3e199a29a9e09c627eaaa2ba203b4a56b67f232
Rolf Rosenbaum <rolf.rosenbaum@rewe-digital.com>
2022-10-14 13:40:50 +0200

    day 5, rewritten and refactored

--- src/main/day05/Day05.kt
+++ src/main/day05/Day05.kt
@@ -1,26 +1,22 @@
 package day05
 
 import readInput
-
 fun part1(input: List<String>): Int {
     return input.first().process().length
 }
 
 fun part2(input: List<String>): Int {
 
-    val foo = ('a'..'z').map { c ->
+    return ('a'..'z').map { c ->
         c to input.first().filterNot {
             c.lowercase() == it.lowercase()
-        }.process().apply { println("$c done") }
-    }
-    val minOfOrNull = foo.minOfOrNull { it.second.length }
-    return minOfOrNull!!
-
+        }.process().apply { println("'$c' done: $length") }
+    }.minOf { it.second.length }
 }
 
 fun main() {
     val input = readInput("main/day05/Day05")
-//    println(part1(input))
+    println(part1(input))
     println(part2(input))
 }
 
@@ -29,20 +25,16 @@ fun String.process(): String {
 }
 
 fun String.processOnce(): Pair<String, Boolean> {
-    var index = 0
-    var result = ""
-    while (index < length) {
-        if (index == this.length - 1) {
-            result += this.last()
-            index++
-        } else if (this[index].isLowerCase() == this[index + 1].isLowerCase()) {
-            result += this[index]
-            index++
-        } else if (this[index].lowercase() != this[index + 1].lowercase()) {
-            result += this[index]
-            index++
-        } else index += 2
-    }
-    return result to (this == result)
 
+    val result = this.fold(mutableListOf<Char>()) { processed, c ->
+        if (processed.lastOrNull() matches c) {
+            processed.removeLast()
+        } else {
+            processed.add(c)
+        }
+        processed
+    }.joinToString("")
+    return result to (result == this)
 }
+
+infix fun Char?.matches(other: Char): Boolean = this != null && this.isLowerCase() != other.isLowerCase() && this.lowercase() == other.lowercase()
---
+++ src/main/day06/Day06.kt
@@ -0,0 +1,19 @@
+package day06
+
+import readInput
+
+fun part1(input: List<String>): Int {
+    return 0
+}
+
+fun part2(input: List<String>): Int {
+    return 0
+
+}
+
+fun main() {
+    val input = readInput("main/day05/Day06")
+//    println(part1(input))
+    println(part2(input))
+}
+
